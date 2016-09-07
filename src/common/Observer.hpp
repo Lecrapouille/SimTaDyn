322b2d166adac6339328acc8138cfa8e0ce4056f
LeCrapouille <Lecrapouille@users.noreply.github.com>
2016-09-07 04:15:22 +0200

    Robustify code for observer/observable

--- src/common/Observer.hpp
+++ src/common/Observer.hpp
@@ -1,17 +1,58 @@
 #ifndef OBSERVER_HPP_
 #  define OBSERVER_HPP_
 
-template <class T, class A>
-class Observer
+template <typename T, class A> class Observable;
+
+// *************************************************************************************************
+// Observer
+// *************************************************************************************************
+template <class T, class A> class Observer
 {
 public:
-  Observer() {}
+  Observer() { observed = NULL; }
   virtual ~Observer() {}
   virtual void update(T& subject, A arg) = 0;
+
+  Observable<T, A>*attached() const
+  {
+    return observed;
+  }
+
+  void attach(Observable<T, A> *new_observed)
+  {
+    if (new_observed != observed)
+      {
+        detach();
+
+        if (new_observed)
+          {
+            std::cout << "Observer::attach\n";
+            observed = new_observed;
+            observed->attachObserver(*this);
+          }
+      }
+  }
+
+  void detach()
+  {
+    if (observed)
+      {
+        std::cout << "Observer::detach\n";
+        Observable<T, A> *old_observed = observed;
+        observed = NULL;
+        old_observed->detachObserver(*this);
+      }
+  }
+
+protected:
+  // http://hillside.net/europlop/HillsideEurope/Papers/EuroPLoP1999/1999_Henney_MutualRegistration.pdf
+  Observable<T, A>* observed;
 };
 
-template <typename T, class A>
-class Observable
+// *************************************************************************************************
+// Observable
+// *************************************************************************************************
+template <typename T, class A> class Observable
 {
 public:
   Observable()
@@ -24,30 +65,47 @@ public:
 
   void attachObserver(Observer<T, A> &observer)
   {
-    // Ensure no duplication.
-    typename std::vector<Observer<T, A> *>::const_iterator it;
+    std::cout << "attachObserver " << &observer << "\n";
 
-    it = std::find(observers_.begin(), observers_.end(), &observer);
-    if (it == observers_.end())
+    if (observer.attached() == this)
       {
-        observers_.push_back(&observer);
+        // Ensure no duplication.
+        typename std::vector<Observer<T, A> *>::const_iterator it;
+        it = std::find(observers_.begin(), observers_.end(), &observer);
+        if (it == observers_.end())
+          {
+            observers_.push_back(&observer);
+          }
+      }
+    else
+      {
+        std::cout << "Observable::attach\n";
+        observer.attach(this);
       }
   }
 
-  void dettachObserver(Observer<T, A> &observer)
+  void detachObserver(Observer<T, A> &observer)
   {
-    typename std::vector<Observer<T, A> *>::const_iterator it;
-
-    it = std::find(observers_.begin(), observers_.end(), &observer);
-    if (it != observers_.end())
+    if (observer.attached() == this)
       {
-        observers_.erase(it);
+        std::cout << "Observable::detach\n";
+        observer.detach();
+      }
+    else
+      {
+        typename std::vector<Observer<T, A> *>::const_iterator it;
+        it = std::find(observers_.begin(), observers_.end(), &observer);
+        if (it != observers_.end())
+          {
+            observers_.erase(it);
+          }
       }
   }
 
-  void dettachAllObservers()
+  void detachAllObservers()
   {
-    observers_.clear();
+    std::cout << "Detach all\n";
+    observers_.clear();//FIXME
   }
 
   virtual void notifyObservers(A arg)
@@ -61,7 +119,7 @@ public:
   }
 
 protected:
-  std::vector<Observer<T, A> *> observers_;
+  std::vector<Observer<T, A> *> observers_; // FIXME containers
 };
 
 #endif /* OBSERVER_HPP_ */
