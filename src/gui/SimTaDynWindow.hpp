aea88b5480fd4d5b807d385d2bb819a2d0a31381
LeCrapouille <Lecrapouille@users.noreply.github.com>
2016-10-02 21:33:14 +0200

    Updating Forth gui

--- src/gui/SimTaDynWindow.hpp
+++ src/gui/SimTaDynWindow.hpp
@@ -2,6 +2,7 @@
 #  define SIMTADYNWINDOW_HPP_
 
 #  include "DrawingArea.hpp"
+#  include <utility>
 #  include <gtkmm.h> // FIXME
 
 class SimTaDynWindow: public Gtk::Window
@@ -18,19 +19,28 @@ public:
   {
     return m_drawing_area.getScreenHeight();
   }
+
 protected:
   void onKeyPressed(GdkEventKey* evenement);
   void onKeyReleased(GdkEventKey* evenement);
+
+  // TODO faire ma propre class notebook/text editor ??
+  std::pair<Gtk::TextView*, std::string> getCurrentTextView();
+  void fileOpen();
+  void fileSave();
+  void fileSaveAs();
+  void fileNew();
 
   GlDrawingArea m_drawing_area;
   Gtk::HPaned m_hpaned[1];
   Gtk::VBox m_vbox[1];
   Gtk::MenuBar m_menubar;
-  Gtk::Menu m_menufile, m_menuedit;
+  Gtk::Menu m_menu[3];
   Gtk::Notebook m_notebook[1];
-  Gtk::ScrolledWindow m_scrolledwindow;
-  Gtk::TextView m_textview;
-  Gtk::Label m_Label1;
+  Gtk::ScrolledWindow m_scrolledwindow[16];
+  Gtk::TextView m_textview[16];
+  Gtk::Image m_menuimage[32];
+  uint32_t m_nb_opened_documents;
 };
 
 #endif /* SIMTADYNWINDOW_HPP_ */
