7c7dfafe82e4b046259b114ec186ebd08ad758a9
LeCrapouille <lecrapouille@gmail.com>
2017-01-24 03:25:02 +0100

    Clean and add iterator for graph container

--- src/SimMap/Cells/GraphMemory.cpp
+++ src/SimMap/Cells/GraphMemory.cpp
@@ -1,16 +1,28 @@
 #include "GraphMemory.hpp"
 
-// Work if and only if M is a power of two number
-#define MODULO(a, b) (a & (b - 1U))
-
 // **************************************************************
-//!
+//! \param no_pools the number of pool of memory to allocated.
 // **************************************************************
 template<class T>
-SimTaDyn::container<T>::container()
+SimTaDyn::container<T>::container(const uint32_t no_pools)
 {
-  m_nth_slot = m_allocated = 0;
-  m_index.push_back(new pool<T>);
+  m_index = m_allocated = 0;
+  reserve(no_pools);
+}
+
+// **************************************************************
+//! \param no_pools the number of pool of memory to allocated.
+// **************************************************************
+template<class T>
+void SimTaDyn::container<T>::reserve(const uint32_t no_pools)
+{
+  uint32_t i = no_pools;
+
+  while (i--)
+    {
+      m_pools.push_back(new poolContainer<T>);
+    }
+  m_allocated += no_pools;
 }
 
 // **************************************************************
@@ -19,35 +31,45 @@ SimTaDyn::container<T>::container()
 template<class T>
 SimTaDyn::container<T>::~container()
 {
-  // TODO: free all memory pools
+  uint32_t i = m_allocated;
+
+  while (i--)
+    {
+      delete m_pools[i];
+    }
+}
+
+// **************************************************************
+//! No check is made to verify if the memory is already used
+//! \param id the index to store the element \c elt.
+//! \param elt the element to store.
+// **************************************************************
+template<class T>
+void SimTaDyn::container<T>::insert(const uint32_t id, T const& elt)
+{
+  const uint32_t index = id / M;
+  const uint32_t subindex = MODULO(id, M);
+
+  // Ressources are full, create at least one new pool memory
+  if (index >= m_allocated)
+    {
+      reserve(1U + index - m_allocated);
+    }
+  insert(index, subindex, elt);
 }
 
 // **************************************************************
 //!
 // **************************************************************
 template<class T>
-void SimTaDyn::container<T>::insert(const uint32_t id, T const& elt)
+void SimTaDyn::container<T>::append(T const& elt)
 {
-  uint32_t index = id / M;
-  uint32_t subindex = MODULO(id, M);
-
-  // The pool memory is full, add a new one
-  while (index > m_allocated)
-    {
-      ++m_allocated;
-      m_index.push_back(new pool<T>);
-    }
-
-  // Insert the new element
-  m_index[index]->m_slots[subindex] = elt;
-
-  // Flag it as 'occupied'
-  m_index[index]->m_occupied[subindex / S]
-    |= (1 << (MODULO(subindex, S)));
+  insert(m_index, elt);
+  ++m_index;
 }
 
 // **************************************************************
-//!
+//! Ignore the case where index > m_allocated
 // **************************************************************
 template<class T>
 void SimTaDyn::container<T>::remove(const uint32_t id)
@@ -57,27 +79,33 @@ void SimTaDyn::container<T>::remove(const uint32_t id)
 
   if (index <= m_allocated)
     {
-      // Flag it as 'not occupied'
-      m_index[index]->m_occupied[subindex / S]
-        &= (~(1 << (MODULO(subindex, S))));
+      remove(index, subindex);
     }
-  // Ignore the case where index > m_allocated
 }
-
-// TODO: remove last
 
 // **************************************************************
 //!
 // **************************************************************
 template<class T>
-bool SimTaDyn::container<T>::isOccupied(const uint32_t id) const
+void SimTaDyn::container<T>::remove()
 {
-  const uint32_t index = id / M;
-  const uint32_t subindex = MODULO(id, M);
+  if (m_index > 0)
+    {
+      --m_index;
+    }
+  remove(m_index);
+}
 
+// **************************************************************
+//!
+// **************************************************************
+template<class T>
+bool SimTaDyn::container<T>::isOccupied(const uint32_t index,
+                                        const uint32_t subindex) const
+{
   if (index <= m_allocated)
     {
-      return 0 != (m_index[index]->m_occupied[subindex / S] &
+      return 0 != (m_pools[index]->m_occupied[subindex / S] &
                    (1 << (MODULO(subindex, S))));
     }
   return true;
@@ -87,18 +115,25 @@ bool SimTaDyn::container<T>::isOccupied(const uint32_t id) const
 //!
 // **************************************************************
 template<class T>
-T* SimTaDyn::container<T>::get(const uint32_t id)
+T* SimTaDyn::container<T>::get(const uint32_t id) const
 {
-  const uint32_t index = id / M;
-  const uint32_t subindex = MODULO(id, M);
+  return get(id / M, MODULO(id, M));
+}
 
+// **************************************************************
+//!
+// **************************************************************
+template<class T>
+T* SimTaDyn::container<T>::get(const uint32_t index,
+                               const uint32_t subindex) const
+{
   if (index < m_allocated)
     {
       // Occuped
-      if (0 != (m_index[index]->m_occupied[subindex / S] &
+      if (0 != (m_pools[index]->m_occupied[subindex / S] &
                 (1 << (MODULO(subindex, S)))))
         {
-          return &(m_index[index]->m_slots[subindex]);
+          return &(m_pools[index]->m_slots[subindex]);
         }
       // Not occuped
       return nullptr;
@@ -113,23 +148,50 @@ T* SimTaDyn::container<T>::get(const uint32_t id)
 template<class T>
 void SimTaDyn::container<T>::debug() const
 {
-  for (uint32_t i = 0; i < m_index.size(); ++i)
+  for (uint32_t pool = 0; pool < m_allocated; ++pool)
     {
-      std::cout << "Slot " << i << ": " << std::endl;
-      pool<T>* e = m_index[i];
+      std::cout << "Pool " << pool << ": " << std::endl;
+      poolContainer<T> *e = m_pools[pool];
 
       // Show elements stored
-      for (uint32_t j = 0; j < M; ++j)
+      for (uint32_t i = 0; i < M; ++i)
         {
-          std::cout << " " << e->m_slots[j];
+          std::cout << " " << e->m_slots[i];
         }
 
       // Show occupation
       std::cout << std::endl;
-      for (uint32_t j = 0; j < M; ++j)
+      for (uint32_t i = 0; i < M; ++i)
         {
-          std::cout << " " << isOccupied(j);
+          std::cout << " " << isOccupied(pool, i);
         }
       std::cout << std::endl;
     }
+}
+
+int main()
+{
+  SimTaDyn::container<int> c(1);
+
+  c.append(41); c.debug();
+  c.append(42); c.debug();
+  c.append(43); c.debug();
+  c.append(44); c.debug();
+  c.append(45); c.debug();
+
+  c.remove(); c.debug();
+  c.remove(); c.debug();
+  c.remove(); c.debug();
+
+  c.append(46); c.debug();
+  c.append(47); c.debug();
+  c.append(48); c.debug();
+
+  std::cout << "Test iterator\n";
+  for (SimTaDyn::container<int>::iterator i = c.begin(); i != c.end(); ++i)
+    {
+      std::cout << "III " << *i << std::endl;
+    }
+
+  return 0;
 }
--- src/SimMap/Cells/GraphMemory.hpp
+++ src/SimMap/Cells/GraphMemory.hpp
@@ -3,45 +3,166 @@
 
 #  include <iostream>
 #  include <vector>
+#  include <cassert>
 
-//! \brief This file implements a class for allocating chunck of
-//! memory specialized for storing graph elements (nodes, links)
-//! where all elements have a unique ID and are stored by ID order
-//! and we desired to avoid allocating and moving memory too much.
+// Work if and only if M is a power of two number
+#define MODULO(a, b) (a & (b - 1U))
+
+//! \brief This file implements a class for allocating pool of memory
+//! specialized for storing graph elements (nodes, links) where their
+//! following particularities is to be stored in order given by their
+//! ID in the aim to have a constant complexity access and avoid
+//! search with complexity O(n) or O(log(n)) with uses more time or
+//! memory ressources. In addition, we try to avoid reallocing
+//! containers: when the container is full, a new one bigger is
+//! created in another portion of the memory, old container is then
+//! copied to the new one and finally old location released. This is
+//! ressource disaster.
 
 namespace SimTaDyn
 {
 
-  //! \class container is a container for graph. It is a collection of
-  //! large but fixed size arrays. The access for adding, removing and
-  //! searching an element is made in a constant time.
+  //! \class container is a container for graph. It's a set of
+  //! allocated but fixed-size arrays (renamed here as pool of memory)
+  //! maintained by an index (an array). So inserting, deleting and
+  //! searching are done in constant time. When ressources are full
+  //! Only allocating an new pool of memory is necessary (old values
+  //! do not need to be copyied). When removing an element, freeing
+  //! pool of memory is not necessary a bool flag indicating if the
+  //! memory is considered as empty.
   template<class T> class container
   {
   public:
-    //! \brief Constructor. Allocate a first pool of element all empty.
-    container();
-    ~container();
-    //! \brief Store an element in the first free slot. Complexity is O(1).
-    //! One pool allocation can be called.
-    inline void append(T const& elt)
+
+    //! \class iterator allows to iterate on element of the container
+    //! and try to be compatible with std::containers iterators.
+    class iterator : public std::iterator<std::input_iterator_tag, T*>
     {
-      insert(m_nth_slot, elt);
-      ++m_nth_slot;
-    }
+    public:
+      typedef T value_type;
+      typedef T& reference;
+      typedef T* pointer;
+      typedef std::forward_iterator_tag iterator_category;
+
+      //! \brief Empty constructor.
+      iterator() : m_container(nullptr), m_itr(0) { }
+      //! \brief Constructor by copy.
+      iterator(const iterator& it) : m_container(it.m_container), m_itr(0) { }
+      //! \brief Constructor from a conatianer
+      iterator(container<T> const& c, const bool end) : m_container(&c)
+      {
+        if (end) m_itr = m_container->size();
+        else m_itr = 0;
+      }
+      //! \brief Iterate on the next occuped element.
+      inline iterator operator++(int)
+      {
+        iterator copy(*this); operator++(); return copy;
+      }
+      //! \brief Iterate on the next occuped element.
+      iterator operator++()
+      {
+        check();
+        do
+          {
+            m_itr++;
+          }
+        while ((m_itr < m_container->size()) &&
+               (false == m_container->isOccupied(m_itr)));
+        return *this;
+      }
+      T& operator*()
+      {
+        check();
+        T* res = m_container->get(m_itr);
+        assert(nullptr != res && "Invalid iterator dereference");
+        return *res;
+      }
+      inline T* operator->()
+      {
+        return m_container->get(m_itr);
+      }
+      inline bool operator==(const iterator& rhs) const
+      {
+        check();
+        return (m_container == rhs.m_container) && (m_itr == rhs.m_itr);
+      }
+      inline bool operator!=(const iterator& rhs) const
+      {
+        return !(*this == rhs);
+      }
+
+    private:
+      inline void check() const { assert(nullptr != m_container && "Uninitialized iterator"); }
+      const container<T>* m_container;
+      uint32_t m_itr;
+    };
+
+  public:
+    //! \brief Constructor. Allocate the X first pools of memory and set
+    //! them as empty.
+    container(const uint32_t no_pools = 1);
+    //! \brief Destructor. Release all allocated pool of memory.
+    ~container();
+    //! \brief Allocate additional pool of memory.
+    void reserve(const uint32_t no_pools);
     //! \brief Store an element at a given index. Complexity is O(1).
-    //! Several pool allocations can be called.
+    //! Several pool allocations can be called if index is greater than
+    //! the current limit of the container.
     void insert(const uint32_t index, T const& elt);
+    //! \brief Same than insert but at th
+    void append(T const& elt);
     //! \brief Remove an element at a given index. Complexity is O(1).
     //! No desallocation is called.
     void remove(const uint32_t index);
+    void remove();
     //! \brief Check if the slot is occupied
-    bool isOccupied(const uint32_t id) const;
+    bool isOccupied(const uint32_t index, const uint32_t subindex) const;
+    inline bool isOccupied(const uint32_t id) const
+    {
+      return isOccupied(id / M, MODULO(id, M));
+    }
     //! \brief Return the element stored at the given index. Complexity is O(1).
-    T* get(const uint32_t id);
+    T* get(const uint32_t id) const;
+    T* get(const uint32_t index, const uint32_t subindex) const;
+    //!
+    iterator begin() const
+    {
+      return iterator(*this, false);
+    }
+    iterator end() const
+    {
+      return iterator(*this, true);
+    }
+    //!
+    inline uint32_t size() const
+    {
+      return m_index;
+    }
     //! \brief Pretty print the memory.
     void debug() const;
 
   private:
+    //!
+    inline void insert(const uint32_t index,
+                       const uint32_t subindex,
+                       T const& elt)
+    {
+      // Insert the new element
+      m_pools[index]->m_slots[subindex] = elt;
+
+      // Flag it as 'occupied'
+      m_pools[index]->m_occupied[subindex / S]
+        |= (1 << (MODULO(subindex, S)));
+    }
+    //!
+    inline void remove(const uint32_t index,
+                       const uint32_t subindex)
+    {
+      // Flag it as 'occupied'
+      m_pools[index]->m_occupied[subindex / S]
+        &= ~(1 << (MODULO(subindex, S)));
+    }
     //! Can be uint64_t. Index is used as 32-bits-field.
     typedef uint32_t Index;
     enum _S { S = sizeof (Index) };
@@ -50,14 +171,23 @@ namespace SimTaDyn
     enum _M { M = 1024U };
     //! Size of the index indicating which slots are occupied.
     enum _I { I = M / S };
+    //!
+    inline uint32_t poolSizeAllocation() const
+    {
+      return M;
+    }
+    inline uint32_t allocated() const
+    {
+      return m_allocated;
+    }
     //! Private class for storing elements in a large array.
-    template<class U> class pool
+    template<class U> class poolContainer
     {
     public:
-      pool()
+      poolContainer()
       {
         // Set all slots are not occupied
-        Index i = I; while (i--) m_occupied[i] = 0;
+        Index i = I; while (i--) { m_occupied[i] = 0; }
       }
       //! Pool of elements: the array is never re-alloced.
       U m_slots[M];
@@ -69,13 +199,12 @@ namespace SimTaDyn
     //! Index of pool memories which can be re-allocated because it
     //! will contain relatively few elements (only addresses on
     //! allocated pool).
-    std::vector<pool<T>*> m_index;
-    //! \brief Number of slots occupied.
-    uint32_t m_nth_slot;
+    std::vector<poolContainer<T>*> m_pools;
+    //! \brief Cursor on the first not occupied element.
+    uint32_t m_index;
     //! \brief Number of memory pool allocated.
     uint32_t m_allocated;
   };
-
 }
 
 #endif /* GRAPHMEMORY_HPP_ */
