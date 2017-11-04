4ab14b0ddb635e227c8a8bbe310322b796f8136b
LeCrapouille <lecrapouille@gmail.com>
2017-11-04 14:54:43 +0100

    Reduce ubuntu packages used in docker

--- .integration/travis-install-linux.sh
+++ .integration/travis-install-linux.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y cmake libgtksourceviewmm-3.0-dev freeglut3 freeglut3-dev mesa-common-dev libglu1-mesa-dev libglew-dev libglfw3-dev libdw-dev libcppunit-dev gcovr libglm-dev libsoil-dev git xvfb checkinstall rpm g++ clang llvm-4.0-tools
+apt-get update
 
+# Global packages
+apt-get install -y git cmake g++ clang
+
+# SimTaDyn dependencies
+apt-get install -y libgtksourceviewmm-3.0-dev freeglut3 freeglut3-dev mesa-common-dev libglu1-mesa-dev libglew-dev libglfw3-dev libdw-dev
+
+# Unit tests dependencies
+apt-get install -y libcppunit-dev libglm-dev llvm-4.0-tools
 ln -s /usr/bin/llvm-symbolizer-4.0 /usr/bin/llvm-symbolizer
