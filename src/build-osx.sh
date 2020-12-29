ba579634b0b3c87161b8965933004add2d5eb5d3
LeCrapouille <lecrapouille@gmail.com>
2020-12-29 18:14:25 +0100

    Fix OS X

--- src/build-osx.sh
+++ src/build-osx.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Install:
+# brew install gtk+ gtkglext mariadb openssl
+
 echo "Cleaning ..."
 rm -fr *~ */*~ SimTaDyn SimTaDyn-generator SimTaDyn-xml2mysql
+
+export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
 
 echo "Compiling SimTaDyn ..."
 gcc -w -Wall -O3 *.c -o SimTaDyn `pkg-config --cflags --libs gtk+-2.0 gtkglext-1.0` -I/usr/local/include -I/opt/X11/include -L/usr/local/lib -I/opt/X11/lib -framework OpenGL -lm -lglib-2.0 -lpangocairo-1.0 -latk-1.0 -lgdk_pixbuf-2.0 -lpango-1.0 -lgmodule-2.0 -lgobject-2.0 -lgthread-2.0 -lcairo `mysql_config --cflags --libs`
